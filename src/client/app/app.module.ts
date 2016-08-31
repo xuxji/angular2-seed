12cbd37bf4d73bf8f5cf0093294bcc7de2209c03
jerry <xuxji.123@163.com>
2016-08-31 18:11:08 +0800

    Add ng2-translate

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -2,16 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http } from '@angular/http';
 import { AppComponent } from './app.component';
 import { routes } from './app.routes';
+import {TranslateModule, TranslateLoader, TranslateStaticLoader} from 'ng2-translate/ng2-translate';
 
 import { AboutModule } from './+about/about.module';
 import { HomeModule } from './+home/home.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, RouterModule.forRoot(routes), AboutModule, HomeModule, SharedModule.forRoot()],
+  imports: [
+      BrowserModule, 
+      HttpModule, 
+      RouterModule.forRoot(routes), 
+      AboutModule, 
+      HomeModule, 
+      SharedModule.forRoot(),
+      TranslateModule.forRoot({
+          provide: TranslateLoader,
+          useFactory: (http: Http) => new TranslateStaticLoader(http, 'assets/i18n', '.json'),
+          deps: [Http]
+      })
+  ],
   declarations: [AppComponent],
   providers: [{
     provide: APP_BASE_HREF,
